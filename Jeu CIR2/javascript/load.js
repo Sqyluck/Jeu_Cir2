6d704ee50cd4e32029ad5aa01b3d74e7d9c25847
VANDAELE Eymeric <eymeric.vandaele@isen-lille.fr>
2016-04-08 17:40:14 +0200

    ups, plus cohérant !

--- Jeu CIR2/javascript/load.js
+++ Jeu CIR2/javascript/load.js
@@ -16,12 +16,12 @@ var loadState = {
 		game.load.spritesheet('player1','assets/Perso1 (1).png',32, 48);
 		game.load.spritesheet('player1dark', 'assets/Perso1 (3).png', 32, 48);
 		game.load.spritesheet('player1dead','assets/Perso1 (2).png',32, 48);
-		game.load.spritesheet('player1deaddark', 'assets/Perso1 (4).png', 32, 48);
+		game.load.spritesheet('player1deaddark', 'assets/Perso1 (3).png', 32, 48);
 
-		game.load.spritesheet('player2','assets/Perso2 (1).png',32, 48);
+		game.load.spritesheet('player2','assets/Perso2 (4).png',32, 48);
 		game.load.spritesheet('player2dark','assets/Perso2 (2).png',32, 48);
 		game.load.spritesheet('player2dead','assets/Perso2 (1).png',32, 48);
-		game.load.spritesheet('player2deaddark','assets/Perso2 (1).png',32, 48);
+		game.load.spritesheet('player2deaddark','assets/Perso2 (3).png',32, 48);
 
 		game.load.spritesheet('player3','assets/fou.png',32, 48);
 		game.load.spritesheet('player3dark','assets/dark-fou2.png',32, 48);
