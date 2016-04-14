cbcaeace15d9867f8a4b11a8d2981995922db742
Luc Fermaut <luc.fermaut@hotmail.fr>
2016-04-14 12:08:41 +0200

    Update viseur.js

--- server/js/viseur.js
+++ server/js/viseur.js
@@ -1,20 +1,20 @@
-var Viseur = function (radius, nbBalle) {
-    this.x = 0;
-    this.y = 0;
+var Viseur = function (radius, nbBalle, pseudo) {
     this.radius = radius;
     this.nbBalle = nbBalle;
+    this.pseudo = pseudo;
 };
 
 Viseur.prototype.constructor = Viseur();
 
-Viseur.prototype.eclairage = function(){
-    mask.drawCircle(0, 0, this.radius);
-    sprite.mask = mask;
-    //game.input.addMoveCallback(this.move, this);
+Viseur.prototype.moveViseur = function(game, socket){
+    this.socket = socket;
+    game.input.addMoveCallback(this.sendCoordonnee, this);
 }
 
-
-Viseur.prototype.move = function(pointer, x, y) {
-    mask.x = x;
-    mask.y = y;
+Viseur.prototype.sendCoordonnee = function(pointer, x, y) {
+    this.socket.emit('moveViseur',{
+        x : x,
+        y : y,
+        pseudo : this.pseudo
+    });
 }
