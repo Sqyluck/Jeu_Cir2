c68fbc932111537b0615c52eea8df73785495895
Anthony Carlier <anthony.carlier@isen-lille.fr>
2016-04-27 13:55:27 +0200

    Update game.js
    Ajout état start + variables globales pour formulaire des options + timer

--- Jeu CIR2/javascript/game.js
+++ Jeu CIR2/javascript/game.js
@@ -2,6 +2,21 @@ var game = new Phaser.Game(window.outerWidth-40, window.outerHeight-110, Phaser.
 
 //Variables Globales
 
+//Choix du BDE
+var choice ='';
+//permet de relancer le solo
+var restart = true;
+
+//Loop time pour solo
+var timer;
+
+//Options par default pour solo
+var timeinit = 60;
+var npcsinit = 10;
+var ammoinit = 1;
+var killerinit = 1;
+
+
 //Declaration des Etats
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
@@ -11,6 +26,7 @@ game.state.add('solo', soloState);
 game.state.add('fin', finState);
 game.state.add('succes', succesState);
 game.state.add('optio', optioState);
+game.state.add('start',startState);
 
 //Lancement du jeu
 game.state.start('boot');
