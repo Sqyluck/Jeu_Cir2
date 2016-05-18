abc0b36f8e070806bc7204baae9035be1f954157
Anthony Carlier <anthony.carlier@isen-lille.fr>
2016-05-18 17:03:32 +0200

    Update settings.js

--- Jeu CIR2/javascript/settings.js
+++ Jeu CIR2/javascript/settings.js
@@ -8,8 +8,18 @@ function getTime(){
 
 }
 
-function getFilms(){
-	filminit = document.getElementsByName('Film') [0].value;
+function getammos(){
+	ammoinit = document.getElementsByName('Ammos') [0].value;
+
+}
+
+function getammos(){
+	ammoinit = document.getElementsByName('Ammos') [0].value;
+
+}
+
+function getammos(){
+	ammoinit = document.getElementsByName('Ammos') [0].value;
 
 }
 
