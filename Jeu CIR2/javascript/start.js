a0cf498db13bdbdfa839113f492f9193d9290fe7
Anthony Carlier <anthony.carlier@isen-lille.fr>
2016-05-18 17:04:49 +0200

    Update start.js

--- Jeu CIR2/javascript/start.js
+++ Jeu CIR2/javascript/start.js
@@ -4,8 +4,8 @@ var startState = {
 		var intro = game.add.text(0, 0, 'Choose a team...',{font: '25px Arial', fill: '#ffffff'});
 		var funcky = game.add.button(game.width/8-100, game.height/2 -200, 'flogo', this.funckymember, this);
         var asterix = game.add.button(100+game.width/2, game.height/2 -180, 'alogo', this.asterixmember, this);
-        funcky.scale.setTo(0.5, 0.5);
-        asterix.scale.setTo(0.45, 0.45);
+        funcky.scale.setTo(0.45, 0.45);
+        //asterix.scale.setTo(1.25, 1.25);
 	},
 
 	funckymember: function() {
