393cb8ef5ad6ab231b612a273bcdf4394cebadcf
Anthony Carlier <anthony.carlier@isen-lille.fr>
2016-05-18 17:05:20 +0200

    Update succes.js

--- Jeu CIR2/javascript/succes.js
+++ Jeu CIR2/javascript/succes.js
@@ -23,13 +23,13 @@ var succesState = {
 			Label3 = game.add.text(game.width/2, achvts.height+150, 'Achvt 3 : ', {font: '25px Arial', fill: '#ffffff'});
 			Label4 = game.add.text(game.width/2, achvts.height+200, 'Achvt 4 : ', {font: '25px Arial', fill: '#ffffff'});
 			home = game.add.button(50, 450, 'fhome', this.restart, this, 0, 1);
-        		home.scale.setTo(0.25, 0.25);
+        	home.scale.setTo(0.25, 0.25);
 
 		}
 		else//------------------Changement BDE----------------------------------------------------------------- 
 		{
-			achvts = game.add.sprite(game.width/2 -150, 0, 'asucces',0);
-			achvts.scale.setTo(0.3, 0.3);
+			achvts = game.add.sprite(game.width/2 -144, 0, 'asucces',0);
+			achvts.scale.setTo(0.7, 0.7);
 
 			bestLabel = game.add.text(50, achvts.height+50, 'Best Score : ', {font: '25px Arial', fill: '#ffffff'});
 			playerLabel = game.add.text(50, achvts.height+100, 'Player : ', {font: '25px Arial', fill: '#ffffff'});
@@ -38,8 +38,8 @@ var succesState = {
 			Label3 = game.add.text(game.width/2, achvts.height+150, 'Achvt 3 : ', {font: '25px Arial', fill: '#ffffff'});
 			Label4 = game.add.text(game.width/2, achvts.height+200, 'Achvt 4 : ', {font: '25px Arial', fill: '#ffffff'});
 			home = game.add.button(game.width/4, 425, 'ahome', this.restart, this, 0, 1);
-			home.scale.setTo(0.2, 0.2);
-	    	}
+	        home.scale.setTo(0.2, 0.2);
+	    }
         
 	},
 
