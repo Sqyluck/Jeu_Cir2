29267bbbd097303d9083e004067850e1c6581cd6
Anthony Carlier <anthony.carlier@isen-lille.fr>
2016-05-18 17:00:10 +0200

    Update game.js

--- Jeu CIR2/javascript/game.js
+++ Jeu CIR2/javascript/game.js
@@ -1,7 +1,10 @@
 var game = new Phaser.Game(window.outerWidth-40, window.outerHeight-110, Phaser.CANVAS,'gameDiv');
-var socket = io.connect('http://169.254.212.87:3000');
+
 //Variables Globales
 
+//Var Map
+var map;
+var layer;
 //Choix du BDE
 var choice ='';
 //permet de relancer le solo
@@ -13,9 +16,12 @@ var timer;
 //Options par default pour solo
 var timeinit = 60;
 var npcsinit = 10;
-var filminit = 1;
+var ammoinit = 1;
 var killerinit = 1;
 
+//Choix lvl
+var lvlrun;
+var lvlunblock = 2;
 
 //Declaration des Etats
 game.state.add('boot', bootState);
@@ -26,6 +32,7 @@ game.state.add('solo', soloState);
 game.state.add('fin', finState);
 game.state.add('succes', succesState);
 game.state.add('optio', optioState);
+game.state.add('level',levelState);
 game.state.add('start',startState);
 
 //Lancement du jeu
