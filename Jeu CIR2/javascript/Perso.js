7d0543e8660bdf85eeeee70009be06d77aa78b1c
VANDAELE Eymeric <eymeric.vandaele@isen-lille.fr>
2016-05-24 21:09:45 +0200

    Update Perso.js

--- Jeu CIR2/javascript/Perso.js
+++ Jeu CIR2/javascript/Perso.js
@@ -24,6 +24,7 @@ var NPC = function(skin){
 
     //temps d'attente de base
     this.wait = game.rnd.between(0, 50);
+    this.Sprite.body.setSize(50, 50, -10, 0);
 
     //point d'arrivé
     this.arriveex = game.rnd.between(50, game.width-50);
@@ -39,7 +40,7 @@ NPC.prototype = Object.create(Phaser.Sprite.prototype);
 NPC.prototype.constructor = NPC;
 NPC.prototype.moveToXY = function(x, y){
     //si le point d'arrivée n'est pas en diagonale, on avance en ligne droite
-    if((Math.abs(this.Sprite.x - this.arriveex) < 5) && (Math.abs(this.Sprite.y - this.arriveey) < 5)){
+    if((Math.abs(this.Sprite.x - this.arriveex) < 10) && (Math.abs(this.Sprite.y - this.arriveey) < 10)){
         return;
     }
     if(Math.abs(Math.abs(x-this.Sprite.x) - Math.abs(y - this.Sprite.y)) > v){
@@ -90,17 +91,35 @@ NPC.prototype.moveToXY = function(x, y){
         }
     }
 }
-NPC.prototype.iaEasy = function(focus){
-    //var target =game.rnd.between(0, my2Array.length-1);
-    this.reelMove(focus);
+NPC.prototype.iaEasy = function(myArray){
+    this.target = game.rnd.between(0, myArray.length-1);
+    console.log(this.target + " " + myArray[this.target].Sprite.x + " " + myArray[this.target].Sprite.y);
+    this.arriveex = myArray[this.target].Sprite.x;
+    this.arriveey = myArray[this.target].Sprite.y;
 }
 
-NPC.prototype.reelMove = function(myArray, nbIA){
+NPC.prototype.iaEasy2 = function(myArray){
+    if(this.detected){
+        this.target = game.rnd.between(0, myArray.length-1);
+        console.log(this.target + " " + myArray[this.target].Sprite.x + " " + myArray[this.target].Sprite.y);
+        this.arriveex = myArray[this.target].Sprite.x;
+        this.arriveey = myArray[this.target].Sprite.y;
+        game.physics.arcade.overlap(this.Sprite, myArray[this.target].Sprite, this.PkillNPC);
+        this.moveToXY(this.arriveex,this.arriveey);
+    }else{
+        this.target = this.closiestIAalive(myArray);
+        console.log(this.target);
+        game.physics.arcade.overlap(this.Sprite, myArray[this.target].Sprite, this.PkillNPC);
+        if((Math.abs(this.Sprite.x - this.arriveex) < 10) && (Math.abs(this.Sprite.y - this.arriveey) < 10))
+            this.findClosePoint();
+        this.moveToXY(this.arriveex, this.arriveey);
+    }
+}
+NPC.prototype.iaMedium = function(myArray, nbIA){
     if(this.target == -1){
         this.target = game.rnd.between(0, npcs-1);
         return;
     }
-   // console.log("is target alive: "+myArray[this.target].alive);
     if(!myArray[this.target].Sprite.alive){
         this.Sprite.animations.stop();
         this.target = -1;
@@ -112,29 +131,39 @@ NPC.prototype.reelMove = function(myArray, nbIA){
     this.moveToXY(this.arriveex, this.arriveey);
 }
 
-//IA MERDIQUE, 1000 RECURSION AVEC this.target = 0
-/*
-NPC.prototype.reelMove2 = function(myArray, nbIA){
-    console.log(nbIA);
-    if(this.target == -1){
-        this.target = 0;
-        for(var i = 0 ; i < nbIA ; i++){
-            //if(Math.sqrt(Math.pow(player.Sprite.x-myArray[i].Sprite.x)+Math.pow(player.Sprite.y-myArray[i].Sprite.y)) < Math.sqrt(Math.pow(player.Sprite.x-myArray[this.target].Sprite.x)+Math.pow(player.Sprite.y-myArray[this.target].Sprite.y))){
-                this.target = i;
-            //}
-        }
+NPC.prototype.iaHard = function(myArray, nbIA){
+    if(this.target != this.closiestIAalive(myArray)){
+        this.target = this.closiestIAalive(myArray);
+        console.log(this.target + " is the target" + myArray[this.target].Sprite.x);
+
     }
     if(!myArray[this.target].Sprite.alive){
-        console.log(this.target);
-        this.target = -1;
-        return this.reelMove2(myArray, nbIA);
+        console.log(this.target + " already dead, new target");
+        this.randomMove();
     }
-    if((Math.abs(this.Sprite.x - this.arriveex) < 5) && (Math.abs(this.Sprite.y - this.arriveey) < 5)){
-        this.findClosePoint();
+    if((game.physics.arcade.distanceToPointer(this.Sprite) > 200)) {
+        this.arriveex = myArray[this.target].Sprite.x;
+        this.arriveey = myArray[this.target].Sprite.y;
+        game.physics.arcade.overlap(this.Sprite, myArray[this.target].Sprite, this.PkillNPC);
     }
-    game.physics.arcade.overlap(this.Sprite, myArray[this.target].Sprite, this.PkillNPC);
+    if((Math.abs(this.Sprite.x - this.arriveex) < 10) && (Math.abs(this.Sprite.y - this.arriveey) < 10)){
+        this.randomMove();
+    }
     this.moveToXY(this.arriveex, this.arriveey);
-}*/
+}
+
+NPC.prototype.closiestIAalive = function(myArray){
+    var ia = 1;
+    while(!myArray[ia].Sprite.alive){ia++;} //on veut une ia en vie pour commencer (il en existe au moins une)
+    var range = Math.sqrt(Math.pow(this.Sprite.x-myArray[ia].Sprite.x)+Math.pow(this.Sprite.y-myArray[ia].Sprite.y));
+    for(var i=1; i<myArray.length ; i++){
+        if(myArray[i].Sprite.alive && Math.sqrt(Math.pow(this.Sprite.x-myArray[i].Sprite.x)+Math.pow(this.Sprite.y-myArray[i].Sprite.y)) < range) {
+            ia = i;
+            range =Math.sqrt(Math.pow(this.Sprite.x-myArray[i].Sprite.x)+Math.pow(this.Sprite.y-myArray[i].Sprite.y));
+        }
+    }
+    return ia;
+}
 
 NPC.prototype.PkillNPC = function (me, Ennemi) {
     //this.animations.stop();
@@ -145,8 +174,6 @@ NPC.prototype.PkillNPC = function (me, Ennemi) {
         npcDisp.setText('Npcs:'+npcsLeft+'/'+npcs);
         console.log(npcsLeft); //a changer ar du son
         this.target =-1;
-        this.arriveex = 0;
-        this.arriveey = 0;
         //this.randomMove();
 
     }
@@ -154,12 +181,17 @@ NPC.prototype.PkillNPC = function (me, Ennemi) {
 
 NPC.prototype.randomMove = function(){
     // si je suis sensé attendre, je réduis ce temps
+    if(this.wait>0){
+        this.wait--;
+        return;
+    }
     this.arriveex = this.Sprite.x;
     this.arriveey = this.Sprite.y;
     this.Sprite.animations.stop();
     var choix = game.rnd.between(0, 100);
     if(choix < 50){
-        this.wait = game.rnd.between(0, 60);
+        this.wait = game.rnd.between(0, 50);
+        return;
     }
     else if(choix < 80){
         this.findClosePoint();
@@ -195,7 +227,7 @@ NPC.prototype.findDistantPoint = function(){
 
 NPC.prototype.IsDetected = function(viseur){
     //var distance = Math.sqrt(Math.pow(this.Sprite.x - viseur.x, 2) + Math.pow(this.Sprite.y - viseur.y, 2) );
-   if( (game.physics.arcade.distanceToPointer(this.Sprite) <= viseur.radius/2) ){
+    if( (game.physics.arcade.distanceToPointer(this.Sprite) <= viseur.radius/2) ){
         if(this.detected == false){
             this.detected = true;
             this.Sprite.name = this.Sprite.name.substring(0, this.Sprite.name.length - 4);
@@ -203,7 +235,6 @@ NPC.prototype.IsDetected = function(viseur){
             this.Sprite.loadTexture(this.Sprite.name);
             this.Sprite.frame = frame;
             animation(this.Sprite);
-
         }
     }else{
         if(this.detected == true){
@@ -220,43 +251,23 @@ NPC.prototype.IsDetected = function(viseur){
 
 NPC.prototype.willDie = function(){
     if(this.Sprite.y > game.height - 40)  this.Sprite.kill();
-    else this.moveToXY(game.width/2, game.height-30);
+    else{
+        this.arriveex = game.width/2;
+        this.arriveey = game.height-30;
+        this.moveToXY(this.arriveex, this.arriveey);
+    }
 }
 
-
-var Player = function(skin){
-    //création du sprite a une position aléatoire
-    this.Sprite = game.add.sprite(game.rnd.between(30, game.width-30), game.rnd.between(30, game.height-30), skin);
-
-    //les differents mouvement du sprite
-    animation(this.Sprite);
-
-
-    //collision
-    game.physics.arcade.enable(this.Sprite,true);
-    //Phillipe je te dédie cette ligne pour que tu puisse mettres la hitbox de nos NPCnnage
-    //...........code ici...........................
-    //this.Sprite.body.setSize(25, 30, 1, 10);
-    this.Sprite.body.collideWorldBounds = true;
-
-    //caracteristique du sprite
-    this.Sprite.anchor.setTo(0.5, 0.5);
-    //this.Sprite.scale.setTo(0.5);
-    this.detected = false;
-};
-
-
-Player.prototype = Object.create(Phaser.Sprite.prototype);
-Player.prototype.constructor = Player;
-
-
-NPC.prototype.movePlayer = function(pad){
-    var left =  (cursors.left.isDown || pad.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1);
-    var right = (cursors.right.isDown || pad.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1);
-    var up =    (cursors.up.isDown || pad.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1) ;
-    var down =  (cursors.down.isDown || pad.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1) ;
+NPC.prototype.movePlayer = function(mypad, myArray, npcs){
+    var killspace = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    var left =  (cursors.left.isDown || mypad.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || mypad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.2);
+    var right = (cursors.right.isDown || mypad.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || mypad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.2);
+    var up =    (cursors.up.isDown || mypad.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || mypad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.2) ;
+    var down =  (cursors.down.isDown || mypad.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || mypad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.2) ;
+    var action =(killspace.isDown || mypad.isDown(Phaser.Gamepad.XBOX360_A));
+    
     // mouvements annulés
-
+    //si opposés
     if(left == true && right == true){
         left = false;
         right = false;
@@ -265,13 +276,20 @@ NPC.prototype.movePlayer = function(pad){
         up = false;
         down = false;
     }
-    
+    // ou si à la limite du terrain
     if(this.Sprite.x > game.width - 20)right = false;
     if(this.Sprite.x < 20) left = false;
     if(this.Sprite.y > game.height - 30)down = false;
     if(this.Sprite.y < 30)up = false;
 
-       //diagonales : 2 input non opposé
+
+    //actions:
+    if(action){
+        for(var i = 0; i < npcs; i++){
+            game.physics.arcade.overlap(player.Sprite, myArray[i].Sprite, player.PkillNPC);
+        }
+    }
+    //diagonales : 2 input non opposé
     if( left && up ){
         this.Sprite.x -= v;
         this.Sprite.y -= v;
