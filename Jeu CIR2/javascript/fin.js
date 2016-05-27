2808f9c75803e4b2657a8970ec6af4fb6477e10c
VANDAELE Eymeric <eymeric.vandaele@isen-lille.fr>
2016-05-27 11:45:03 +0200

    Update fin.js

--- Jeu CIR2/javascript/fin.js
+++ Jeu CIR2/javascript/fin.js
@@ -12,24 +12,28 @@ var finState = {
 		//Remise à zéro des count
 		restart = true;
 		console.log(npcsLeft*100/npcs);
-		if(npcsLeft*100/npcs>75 && killersLeft==0 && lvlunblock <= lvlrun) {
-			if(lvlrun < 6){
-				lvlunblock = lvlrun + 1;
-				var text = game.add.text(50, winner.height+210, 'LEVEL '+lvlunblock+' UNLOCK !', {font: '25px Arial', fill: '#ffffff'});
+		if(lvlrun>0){
+			if(npcsLeft*100/npcs>75 && killersLeft==0 && lvlunblock <= lvlrun) {
+				if(lvlrun < 6){
+					lvlunblock = lvlrun + 1;
+					var text = game.add.text(50, winner.height+210, 'LEVEL '+lvlunblock+' UNLOCK !', {font: '25px Arial', fill: '#ffffff'});
+				}else{
+					var text = game.add.text(50, winner.height+210, "You finally discovered the evil plan of the Asterix’en. ",{font: '25px Arial', fill: '#ffffff'});
+					var text = game.add.text(50, winner.height+250, "Unfortunately, no one really care about your thought so they eventually became the new official BDE of ISEN.",{font: '25px Arial', fill: '#ffffff'});
+					var text = game.add.text(50, winner.height+290, "But it’s always great to find the truth when people lie to you, even if sometimes it hurts.",{font: '25px Arial', fill: '#ffffff'}); 
+					var text = game.add.text(50, winner.height+330, "However, you can play with your friend and try the multiplayer mode." , {font: '25px Arial', fill: '#ffffff'});
+	
+				}
+			}else if(npcsLeft*100/npcs<=75) {
+				var text = game.add.text(50, winner.height+210, 'YOU LET TOO MUCH STUDENTS GET ASTERIXED, TRY AGAIN !', {font: '25px Arial', fill: '#ffffff'});
+			}else if(killersLeft>0) {
+				var text = game.add.text(50, winner.height+210, "YOU DIDN'T TAKE A PICTURE OF BDE'S MEMBER, TRY AGAIN !", {font: '25px Arial', fill: '#ffffff'});
 			}else{
-				var text = game.add.text(50, winner.height+210, "You finally discovered the evil plan of the Asterix’en. ",{font: '25px Arial', fill: '#ffffff'});
-				var text = game.add.text(50, winner.height+250, "Unfortunately, no one really care about your thought so they eventually became the new official BDE of ISEN.",{font: '25px Arial', fill: '#ffffff'});
-				var text = game.add.text(50, winner.height+290, "But it’s always great to find the truth when people lie to you, even if sometimes it hurts.",{font: '25px Arial', fill: '#ffffff'}); 
-				var text = game.add.text(50, winner.height+330, "However, you can play with your friend and try the multiplayer mode." , {font: '25px Arial', fill: '#ffffff'});
-
+				var text = game.add.text(50, winner.height+210, 'WOW, AGAIN A GOOD JOB ON LEVEL '+lvlrun+' !', {font: '25px Arial', fill: '#ffffff'});
+	
 			}
-		}else if(npcsLeft*100/npcs<=75) {
-			var text = game.add.text(50, winner.height+210, 'YOU LET TOO MUCH STUDENTS GET ASTERIXED, TRY AGAIN !', {font: '25px Arial', fill: '#ffffff'});
-		}else if(killersLeft>0) {
-			var text = game.add.text(50, winner.height+210, "YOU DIDN'T TAKE A PICTURE OF BDE'S MEMBER, TRY AGAIN !", {font: '25px Arial', fill: '#ffffff'});
 		}else{
-			var text = game.add.text(50, winner.height+210, 'WOW, AGAIN A GOOD JOB ON LEVEL '+lvlrun+' !', {font: '25px Arial', fill: '#ffffff'});
-
+			var text = game.add.text(50, winner.height+210, 100-(npcsLeft*100/npcs)+'% ASTERIXED !', {font: '25px Arial', fill: '#ffffff'});
 		}
 		var startLabel = game.add.text(game.width/2, game.height -40, 'Press SPACE', {font: '25px Arial', fill: '#ffffff'});
 		var wkey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
