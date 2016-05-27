492a8458519ac5524140304a3b9ea5676c6ec4d6
VANDAELE Eymeric <eymeric.vandaele@isen-lille.fr>
2016-05-27 10:02:29 +0200

    Update load.js

--- Jeu CIR2/javascript/load.js
+++ Jeu CIR2/javascript/load.js
@@ -73,7 +73,7 @@ var loadState = {
 		game.load.spritesheet('photo','assets/camera.png',89,87,5);
 		
 		//Background
-		game.load.image('ascenseur', 'assets/background/floor.jpg');
+		game.load.image('ascenseur', 'assets/background/floor.png');
 
 		//Filtres
 		game.load.image('filtreSombre', 'assets/filtreSombre.png');
