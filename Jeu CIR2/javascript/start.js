53768526a89fd90dedfe828d75845ef2933277d7
VANDAELE Eymeric <eymeric.vandaele@isen-lille.fr>
2016-05-27 10:24:05 +0200

    Update start.js

--- Jeu CIR2/javascript/start.js
+++ Jeu CIR2/javascript/start.js
@@ -3,6 +3,8 @@ var startState = {
 
 		var playOnline = game.add.button(game.width/2-100, game.height/3, 'playOnline', this.online, this, 1, 0);
 		var playOffline = game.add.button(game.width/2-100, 2*game.height/3, 'playOffline', this.offline, this, 1, 0);
+		var home = game.add.button(20, game.height/2, 'back', this.restart, this, 0, 1);
+
 	},
 
 	online: function() {
@@ -13,5 +15,8 @@ var startState = {
 	offline: function() {
     	
     	game.state.start('optio');
+	},	
+	restart: function() {
+		game.state.start('menu');
 	}
 };
